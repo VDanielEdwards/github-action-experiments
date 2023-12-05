4f549287defd733df13c52efc7c31a8e4fbaa217
Daniel Edwards <Daniel.Edwards@vector.com>
2023-12-05 12:59:19 +0000

    -- add docker-job experiment

---
+++ .github/actions/docker-job/docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+runner_uid=$(ls -lnd . | awk '{ print $3; }')
+runner_gid=$(ls -lnd . | awk '{ print $4; }')
+
+echo "Runner UID: ${runner_uid}"
+echo "Runner GID: ${runner_gid}"
+
+useradd --uid "${runner_uid}" --no-user-group --home-dir "$(pwd)" --no-create-home worker
+
+su -c scripts/run.sh - worker
+
---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Environment Variables:"
+env
+
